bf1a366fe4a07401ef33fbb992223124b2274000
Shrief Essam <shriefessam1999@gmail.com>
2024-05-04 20:55:41 +0300

    change navbar content if user logged in

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -2,8 +2,17 @@ import logo from "@/assets/logo.png";
 import { Button } from "../ui/button";
 import { NavLink } from "react-router-dom";
 import { Search } from "lucide-react";
+import { useTypedSelector } from "@/store";
+import { Avatar, AvatarFallback } from "../ui/avatar";
 
 const Navbar = () => {
+  const user = useTypedSelector((state) => state.authState.user);
+
+  const logout = () => {
+    localStorage.removeItem("refreshToken");
+    window.location.reload();
+  };
+
   return (
     <div className="w-full bg-card font-Inter grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-y-6 py-4 px-4 md:px-8 lg:48 xl:px-60 md:gap-y-0">
       <div className="order-2 md:order-1 md:col-span-2 lg:col-span-1 flex items-center gap-12">
@@ -17,17 +26,38 @@ const Navbar = () => {
       </div>
       <div className="order-1 md:order-2 md:col-span-1 justify-between md:justify-end flex items-center gap-4">
         <div className="flex gap-4 items-center order-2 md:order-1">
-          <NavLink to="/login">
-            <Button variant="ghost" className="font-semibold px-0 md:px-4">
-              Login
-            </Button>
-          </NavLink>
+          {!user && (
+            <>
+              <NavLink to="/login">
+                <Button variant="ghost" className="font-semibold px-0 md:px-4">
+                  Login
+                </Button>
+              </NavLink>
 
-          <NavLink to="/signup">
-            <Button variant="ghost" className="font-semibold px-0 md:px-4">
-              Sign up
-            </Button>
-          </NavLink>
+              <NavLink to="/signup">
+                <Button variant="ghost" className="font-semibold px-0 md:px-4">
+                  Sign up
+                </Button>
+              </NavLink>
+            </>
+          )}
+
+          {user && (
+            <>
+              <Button
+                variant="ghost"
+                className="font-semibold px-0 md:px-4"
+                onClick={logout}
+              >
+                Logout
+              </Button>
+              <Avatar>
+                <AvatarFallback>
+                  {user.fullName.slice(0, 2).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+            </>
+          )}
         </div>
 
         <div className="flex flex-col gap-1  md:px-4 order-1 md:order-2">
